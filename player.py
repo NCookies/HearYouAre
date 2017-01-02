998a0e13d5b2ba2203ebd0278bb9426174ee2400
NCookie <swstar21c@gmail.com>
2017-01-03 01:57:56 +0900

    Complete to make a music player. Usually, it do not have a problem, but if the music was changed or removed when that is in music queue. To solve this, I think update_queue function should be multiprocessing.

--- player.py
+++ player.py
@@ -4,18 +4,31 @@ import pygame
 import os
 import Queue
 import memcache
+import time
 
 
-class Player:
+NO_MORE_MUSIC = 234
+
+pygame.mixer.music.set_endevent()
+
+
+class MusicPlayer:
     """
     음악 재생을 하기 위해 만든 클래스
     디렉터리 내의 파일들을 큐에 넣어서 재생함
     일정 주기를 가지고 큐를 업데이트 (새로운 파일이 추가되었을 때에만)
     """
-    def __init__(self):
-        self.dir_path = os.getcwd() + '/res/music'
+    def __init__(self, music_path):
+        # root/res/music
+        self.dir_path = os.getcwd() + music_path
         self.music_queue = Queue.Queue()
         self.mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        self.make_list()
+
+        try:
+            pygame.mixer.init(44100)
+        except pygame.error:
+            print('Could not initialise audio.')
 
     def make_list(self):
         """
@@ -23,8 +36,12 @@ class Player:
         현재 디렉터리 안에 있는 모든 파일 포함
         :return:
         """
-        pass
+        music_list = os.listdir(self.dir_path)
+        music_list.sort()
+        for music in music_list:
+            self.music_queue.put(music)
 
+    # 나중에 음악의 개수가 많아지면 이 부분만 따로 멀티프로세싱으로 돌려야 할 듯
     def update_queue(self):
         """
         재생할 음악의 큐를 업데이트함
@@ -35,9 +52,11 @@ class Player:
         file_list.sort()
         music_list = self.music_queue.queue
 
+        # 기존의 큐와 새로 얻어온 음악 리스트가 같을 경우 패스
         if len(music_list) == len(file_list):
-            pass
+            return
 
+        # 기존의 큐와 새로 만든 리스트가 중복되지 않은 값들을 얻어냄
         updates = list()
         for i in range(0, len(file_list)):
             print i
@@ -48,22 +67,56 @@ class Player:
                 updates.append(file_list[i])
                 continue
 
-        self.music_queue.put([updates[x] for x in updates])
+        updates.sort()
+        for new in updates:
+            self.music_queue.put(new)
 
-    def play_music(self, song):
-        self.play_music(song)
-        pygame.init()
-        pygame.mixer.music.load(song)
-        pygame.mixer.music.play()
+    def play_music(self):
+        if not pygame.mixer.get_init():
+            return
 
-        while pygame.mixer.music.get_busy():
-            self.mc.set("now_play", "")  # 현재 재생 중인 음악
-            self.mc.set("play_time", "")  # 음악 재생 시간
+        while True:
+            try:
+                # 큐에 아무것도 없다면 계속 루프를 돌면서 큐 업데이트
+                # 뒤의 Empty 예외가 catch 되지 않아서 이런 방식을 사용함
+                # 이에 대해서는 추후에 다시 알아봐야 할 듯
+                if len(list(self.music_queue.queue)) <= 0:
+                    time.sleep(3)
+                    self.update_queue()
+                    continue
 
-        # 음악 플레이 큐 업데이트
-        self.update_queue()
+                song = self.dir_path + '/' + self.music_queue.get()
+                print song
+            # 예외 처리 루틴에 들어가지지 않음
+            except Queue.Empty:
+                self.mc.set("now_play", "none")  # 큐에 음악이 없을 때
+                self.mc.set("play_time", "none")
+                # 큐에 아무것도 없다면 잠깐 딜레이를 준 후에 다시 확인
+                time.sleep(3)
+                self.update_queue()
+                continue
+
+            self.mc.set("now_play", song)  # 현재 재생 중인 음악
+            try:
+                pygame.mixer.music.load(song)
+            except pygame.error:
+                self.update_queue()
+            pygame.mixer.music.play()
+
+            # 음악이 종료될 때까지 재생
+            while pygame.mixer.music.get_busy():
+                # 음악 재생 시간
+                self.mc.set("play_time",
+                            int(pygame.mixer.music.get_pos()) / 1000)
+                # 1초마다 재생 중인 음악의 재생 시간 업데이트
+                time.sleep(1)
+
+            # 음악이 모두 플레이되었다면 삭제
+            os.remove(song)
+            # 음악 플레이 큐 업데이트
+            self.update_queue()
 
 
-li = os.listdir(os.getcwd() + '/res/music')
-li.sort()
-print li
+if __name__ == "__main__":
+    player = MusicPlayer('/res/music')
+    player.play_music()
